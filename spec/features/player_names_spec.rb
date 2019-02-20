f3afae77dc0892e16adf6f3b36885c8fb0a160eb
Maria Vanaale <mvanaale@gmail.com>
2019-02-20 15:29:25 +0000

    post added

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -3,8 +3,8 @@ require './app'
 feature 'player names' do
   scenario 'players enter names' do
     visit('/')
-    fill_in 'player1', with: 'Maria'
-    fill_in 'player2', with: 'Sarah'
+    fill_in :player1, with: 'Maria'
+    fill_in :player2, with: 'Sarah'
     click_button 'Submit'
     expect(page).to have_content("Maria")
  end
