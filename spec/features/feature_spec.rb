38ceb8011cd0354a49188c20d0fb809d9d25faff
Maria Vanaale <mvanaale@gmail.com>
2019-02-20 14:19:47 +0000

    testing with capybara

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,13 +1,8 @@
 require './app'
 
-describe Battle do
-
-it '....' do
-
-  visit('/')
-  expect(page).to have_content("Testing infrastructure working!")
-end
-
-
-
+feature 'Testing infrastructure' do
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content("Testing infrastructure working!")
+ end
 end
